3ae18d80f8f0718c7d5871136ff7d39e60c3e4fd
Swizec Teller <swizec@swizec.com>
2017-10-01 16:27:43 -0700

    Explainer text

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ class App extends Component {
         return (
             <div>
                 <MortysMindblowers />
+                <p style={{color: 'white', textAlign: 'center'}}>
+                    Built by Swizec Teller during a <a href="https://www.youtube.com/watch?v=a4g81widnGM" style={{color: 'white'}}>Livecoding Session</a>. Morty's Mindblowers for gifs from Imgur's frontpage. <a href="https://github.com/Swizec/mortys-mindblowers" style={{color: 'white'}}>Code on Github</a>.
+                </p>
             </div>
         );
     }
