36caae3181687b561d5f546896afd6df6954afe2
Swizec Teller <swizec@swizec.com>
2018-02-11 23:24:40 -0800

    Use styledComponents in App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
 
-import MortysMindblowers from './MortysMindblowers';
+import MortysMindblowers from "./MortysMindblowers";
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+`;
+
+const WhiteP = styled.p`
+    color: white;
+    textalign: center;
+`;
+
+const WhiteA = styled.a`
+    color: "white";
+`;
 
 class App extends Component {
     render() {
         return (
-            <div style={{display: 'flex', flexDirection: 'column',
-                         justifyContent: 'center', alignItems: 'center', height: '100%'}}>
+            <Container>
                 <MortysMindblowers />
 
-                <p style={{color: 'white', textAlign: 'center'}}>
-                    Built by Swizec Teller during a <a href="https://www.youtube.com/watch?v=a4g81widnGM" style={{color: 'white'}}>Livecoding Session</a>. Morty's Mindblowers for gifs from Imgur's frontpage. <a href="https://github.com/Swizec/mortys-mindblowers" style={{color: 'white'}}>Code on Github</a>.
-                </p>
-            </div>
+                <WhiteP>
+                    Built by Swizec Teller during a{" "}
+                    <WhiteA href="https://www.youtube.com/watch?v=a4g81widnGM">
+                        Livecoding Session
+                    </WhiteA>. Morty's Mindblowers for gifs from Imgur's
+                    frontpage.{" "}
+                    <WhiteA href="https://github.com/Swizec/mortys-mindblowers">
+                        Code on Github
+                    </WhiteA>.
+                </WhiteP>
+            </Container>
         );
     }
 }
